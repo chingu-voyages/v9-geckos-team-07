4fdd198311f1fdfec0ff8ca3ec0bffd52977300f
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-07 23:16:27 -0400

    tests: updated app tests
    not sure why, but adding in jest-express seems to have made it so the
    test don't always hang. still can't figure out it's api though.

--- server/app.test.js
+++ server/app.test.js
@@ -1,21 +1,32 @@
+const { Express } = require('jest-express/lib/express')
+
 const request = require('supertest')
-const { app, mongoose } = require('./app')
 
-afterEach(async done => {
-  await mongoose.disconnect()
+const { app: server, mongoose } = require('./app')
 
-  done()
-})
+jest.mock('morgan', () => jest.fn(() => (req, res, next) => next()))
 
 describe('server tests', () => {
+  let app
+  beforeEach(() => {
+    app = new Express()
+  })
+
+  afterEach(async done => {
+    await mongoose.disconnect()
+    app.resetMocked()
+
+    done()
+  })
+
   test('should return 200 from base route', async () => {
-    const response = await request(app).get('/')
+    const response = await request(server).get('/')
 
     expect(response.status).toBe(200)
   })
 
   test('/auth/google should redirect to google', async () => {
-    const response = await request(app).get('/auth/google')
+    const response = await request(server).get('/auth/google')
 
     expect(response.redirect).toBeTruthy()
     expect(response.statusCode).toBe(302)
@@ -25,7 +36,7 @@ describe('server tests', () => {
   })
 
   test('/auth/google/callback should redirect', async () => {
-    const response = await request(app).get('/auth/google/callback')
+    const response = await request(server).get('/auth/google/callback')
     expect(response.redirect).toBeTruthy()
     expect(response.statusCode).toBe(302)
   })
