48fe14ecd8007d58505deebdbb2e338de79c5394
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-11 08:33:10 -0400

    feat: added enum to account type
    Account types may only be one of 'Asset', 'Expense', 'Liability',
    'Income', 'Bank', 'Cash', 'Credit Card', 'Stock', 'Mutial Fund',
    'A/Receivable', or 'A/Payable'. No other strings should be allowed.

--- server/models/accounts.js
+++ server/models/accounts.js
@@ -5,9 +5,26 @@ const { Schema } = mongoose
 const accountSchema = new Schema({
   _parent: { type: Schema.Types.ObjectId, ref: 'Account' },
   name: String,
-  type: String,
+  type: {
+    type: String,
+    enum: [
+      'Asset',
+      'Expense',
+      'Liability',
+      'Income',
+      'Bank',
+      'Cash',
+      'Credit Card',
+      'Stock',
+      'Mutual Fund',
+      'A/Receivable',
+      'A/Payable'
+    ]
+  },
   description: String,
   currency: String
 })
+
+accountSchema.pre('save', () => {})
 
 module.exports = accountSchema
