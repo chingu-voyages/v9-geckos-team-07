d703f98d3e8eadb992260be53ed2b1cd4419567d
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-11 08:35:25 -0400

    feat: splits will not accept negative numbers

--- server/models/splits.js
+++ server/models/splits.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose
 
 const splitSchema = new Schema({
   account: { type: Schema.Types.ObjectId, ref: 'Account' },
-  in: { type: Number, default: 0 },
-  out: { type: Number, default: 0 }
+  in: { type: Number, default: 0, min: 0 },
+  out: { type: Number, default: 0, min: 0 }
 })
 
 module.exports = splitSchema
