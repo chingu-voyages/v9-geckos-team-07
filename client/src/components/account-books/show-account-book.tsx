9aafc3c3440080d48bee507bb641179cb663016e
Joseph Morse <tamed.lionheart@gmail.com>
2019-06-27 09:32:55 -0400

    fix: mapStateToProps now only returns accounts
    now only returns the accounts the component cares about

--- client/src/components/account-books/show-account-book.tsx
+++ client/src/components/account-books/show-account-book.tsx
@@ -2,31 +2,25 @@ import React, { Component } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { StoreState } from '../../reducers';
-import { CompleteAccountBook, CompleteAccount } from '../../actions';
+import { CompleteAccount } from '../../actions';
 import { StateStore } from 'passport-oauth2';
 
 interface AccountBookProps extends RouteComponentProps<{ title: string }> {
-  accountBooks: CompleteAccountBook[];
+  accounts: CompleteAccount[];
 }
 
 type Props = StateStore & AccountBookProps;
 
 export class ShowAccountBook extends Component<Props> {
-  render(): JSX.Element {
+  private sortAccounts = (): null => {
+    return null;
+  };
+
+  public render(): JSX.Element {
     const { title } = this.props.match.params;
-    let accounts: JSX.Element[] = [];
-
-    const accountBook = this.props.accountBooks.find(
-      item => item.title === title
+    const accounts: JSX.Element[] = this.props.accounts.map(
+      (account): JSX.Element => <div key={account._id}>{account.name}</div>
     );
-
-    if (accountBook) {
-      accounts = accountBook.accounts.map(
-        (item: CompleteAccount): JSX.Element => (
-          <div key={item._id}>{item.name}</div>
-        )
-      );
-    }
 
     return (
       <>
@@ -40,15 +34,29 @@ export class ShowAccountBook extends Component<Props> {
   }
 }
 
+/**
+ * I didn't even know you could do this!
+ *
+ * @param param0 Peel off Props
+ * @param ownProps Component Props
+ */
 function mapStateToProps(
   { user: { accountBooks } }: StoreState,
   ownProps: AccountBookProps
-): { accountBooks: CompleteAccountBook[] } {
-  return { accountBooks };
+): { accounts: CompleteAccount[] } {
+  const accountBook = accountBooks.find(
+    item => item.title === ownProps.match.params.title
+  );
+
+  if (accountBook) {
+    return { accounts: accountBook.accounts };
+  }
+
+  return { accounts: [] };
 }
 
 export const ConnectedShowAccountBook = connect<
-  { accountBooks: CompleteAccountBook[] }, // State pulled off
+  { accounts: CompleteAccount[] }, // State pulled off
   {}, // dispatch if any
   AccountBookProps, // Props passed to component
   StoreState // Store State
