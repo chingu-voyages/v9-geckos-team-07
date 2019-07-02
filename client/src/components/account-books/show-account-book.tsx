4c6a47953d61d89a2dab4dc78ca5221ab28f2158
Joseph Morse <tamed.lionheart@gmail.com>
2019-07-02 17:27:31 -0400

    fix: arounged accounts in a tier format
    arranged them in a tier based on parent and children.

--- client/src/components/account-books/show-account-book.tsx
+++ client/src/components/account-books/show-account-book.tsx
@@ -1,34 +1,78 @@
 import React, { Component } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { StoreState } from '../../reducers';
 import { CompleteAccount } from '../../actions';
-import { StateStore } from 'passport-oauth2';
 
 interface AccountBookProps extends RouteComponentProps<{ title: string }> {
   accounts: CompleteAccount[];
 }
 
-type Props = StateStore & AccountBookProps;
+type Props = StoreState & AccountBookProps;
 
 export class ShowAccountBook extends Component<Props> {
-  private sortAccounts = (): null => {
-    return null;
-  };
+  private hasChildren(account: CompleteAccount): boolean {
+    return this.props.accounts.some(a => a.parent === account._id);
+  }
+
+  private getChildren(account: CompleteAccount): CompleteAccount[] {
+    return this.props.accounts.reduce((acc: CompleteAccount[], child) => {
+      if (account._id === child.parent) {
+        return [...acc, child];
+      }
+
+      return acc;
+    }, []);
+  }
+
+  private renderAccounts(accounts: CompleteAccount[]): JSX.Element[] {
+    return accounts.reduce((acc: JSX.Element[], account, idx, arr) => {
+      if (this.hasChildren(account)) {
+        const children = this.getChildren(account);
+
+        // remove children from array
+        children.forEach(child => {
+          arr.splice(arr.findIndex(f => f._id === child._id), 1);
+        });
+
+        // render children
+        const results = this.renderAccounts(children);
+
+        const element = (
+          <li key={account._id}>
+            <div>{account.name}</div>
+            <ul>{results}</ul>
+          </li>
+        );
+
+        return [...acc, element];
+      }
+
+      const element = (
+        <li key={account._id}>
+          <Link to={`/accounts/${account._id}`}>{account.name}</Link>
+        </li>
+      );
+
+      return [...acc, element];
+    }, []);
+  }
 
   public render(): JSX.Element {
-    const { title } = this.props.match.params;
-    const accounts: JSX.Element[] = this.props.accounts.map(
-      (account): JSX.Element => <div key={account._id}>{account.name}</div>
-    );
-
+    const {
+      accounts,
+      match: {
+        params: { title }
+      }
+    } = this.props;
     return (
       <>
         <header>
           <h2>{title}</h2>
         </header>
 
-        {accounts}
+        <ul>{this.renderAccounts([...accounts])}</ul>
       </>
     );
   }
